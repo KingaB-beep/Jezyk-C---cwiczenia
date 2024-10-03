06aa7992f3b452dbd13303e664b5e27ef58c8ff9
Kinga Berdzik <kingaberdzik0@gmail.com>
2024-10-03 20:36:48 +0200

    Zadanie_2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Kinga Berdzik\n");
-	printf("Kinga\nBerdzik\n");
-	printf("Kinga ");
-	printf("Berdzik");
+	printf("Imie i nazwisko: Kinga Berdzik\n");
+	printf("Adres: ul. Kwiatowa 10/10\n01-001 Warszawa, Polska");
 	return 0;
 	
+
 
 }
