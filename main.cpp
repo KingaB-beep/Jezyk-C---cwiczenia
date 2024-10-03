41debf9c4c214ce34bcb0136bf300115c3e46cdd
Kinga Berdzik <kingaberdzik0@gmail.com>
2024-10-03 21:29:36 +0200

    zadanie_4

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <stdio.h>
+void PanJan(void);
+void OstatniWiersz(void);
+
 int main(void)
 {
-	int WiekwLatach, WiekwDniach;
-
-	WiekwLatach = 25;
-	WiekwDniach = WiekwLatach * 365;
-	printf("%d lat to %d dni\n", WiekwLatach, WiekwDniach);
+	PanJan();
+	PanJan();
+	OstatniWiersz();
 	return 0;
-	
+}
 
+void PanJan(void)
+{
+	printf("Panie Janie!\n");
+}
 
+void OstatniWiersz(void)
+{
+	printf("Rano wstan!");
 }
